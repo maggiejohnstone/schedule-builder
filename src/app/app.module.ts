711143b4230e7cd96f1853327784ecf7551a5483
maggiejohnstone <m.nichole.j@gmail.com>
2021-02-19 21:06:33 -0600

    Add top bar component and adjust global app styling
    Co-authored-by: Flav <flaviuvadan@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
