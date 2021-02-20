86b01deebabec006a6cccd0bc0d3c529d851af43
maggiejohnstone <m.nichole.j@gmail.com>
2021-02-19 23:00:55 -0600

    Add global styling, render schedule list items and material ui Co-authored-by: Flav <flaviuvadan@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { TopbarComponent } from './topbar/topbar.component';
-import { ScheduleComponent } from './schedule/schedule.component';
-import { ScheduleListComponent } from './home/schedule-list/schedule-list.component';
-import { ScheduleListItemComponent } from './home/schedule-list/schedule-list-item/schedule-list-item.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {TopbarComponent} from './topbar/topbar.component';
+import {ScheduleComponent} from './schedule/schedule.component';
+import {ScheduleListComponent} from './home/schedule-list/schedule-list.component';
+import {ScheduleListItemComponent} from './home/schedule-list/schedule-list-item/schedule-list-item.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -20,9 +22,12 @@ import { ScheduleListItemComponent } from './home/schedule-list/schedule-list-it
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
